d4a5a7c79eacfb7421085f3a29b3716152d45eb3
gleb-tregubov <tregubovgleb@gmail.com>
2021-07-05 15:39:24 +0300

    Initial Commit

---
+++ rs.ios.stage-task7/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  rs.ios.stage-task7
+//
+//  Created by Gleb Tregubov on 05.07.2021.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ rs.ios.stage-task7/ViewController.m
@@ -0,0 +1,22 @@
+//
+//  ViewController.m
+//  rs.ios.stage-task7
+//
+//  Created by Gleb Tregubov on 05.07.2021.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
